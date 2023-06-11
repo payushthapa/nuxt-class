ec9eed96770f185572a12397a948841ed26358d8
payushthapa <thapapayush2@gmail.com>
2023-06-11 02:32:13 -0700

    hero and card edit

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,57 @@
 <template>
-    <div>
-        <h1>this is home page</h1>
-    </div>
+<div>
+    <section class="hero py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>welcome to home page</h1>
+                    <p>ristiano Ronaldo dos Santos Aveiro GOIH ComM is a Portuguese professional footballer who plays as a forward for and captains both Saudi Professional League club Al Nassr and the Portugal national team.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- card -->
+    <section class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="https://cdn.pixabay.com/photo/2023/06/04/05/27/motorcycle-8038868__340.jpg" alt="" width="400px">
+                           <h1>this is home made image</h1>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <img src="https://cdn.pixabay.com/photo/2023/06/04/05/27/motorcycle-8038868__340.jpg" alt="" width="400px">
+                            </div>
+                            <div class="col-md-6">
+                                <p>cristiano Ronaldo dos Santos Aveiro GOIH ComM is a Portuguese professional footballer who plays as a forward for and captains both Saudi Professional League club Al Nassr and the Portugal national team.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
 </template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.hero{
+    background-color: blue;
+    text-align: center;
+    height: 400px;
+}
+.card{
+    border-radius: 10px;
+    overflow: hidden;
+    box-shadow: 10px 10px 10px 10px gray;
+}
+</style>
