d7a22b60728fdc9829b24361b61ed8b4184bba7a
payushthapa <mingsomglimbu@gmail.com>
2023-06-12 02:32:52 -0700

    product made

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,25 +10,18 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <nuxt-link to="/" class="nav-link">Home</nuxt-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+          <nuxt-link to="/About" class="nav-link">About Us</nuxt-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <nuxt-link to="/contact" class="nav-link">Contact Us</nuxt-link>
         </li>
+        <li class="nav-item">
+          <nuxt-link to="/service" class="nav-link">Service</nuxt-link>
+        </li> 
+        
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
--- pages/sevice.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>this is sevice page</h1>
-    </div>
-</template>
