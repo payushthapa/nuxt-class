f8ca4a777cb3c62ac298d7f9ec780b9bfd93aec1
payushthapa <mingsonglimbu@gmail.com>
2023-06-13 15:05:52 +0545

    id added

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+       <h1>product Detailed Page</h1> 
+       <h2>The product number is{{ route.params }}</h2>
+    </div>
+</template>
+
+<script setup>
+const route = useRoute()
+</script>
+
+<style lang="scss" scoped>
+
+</style>
