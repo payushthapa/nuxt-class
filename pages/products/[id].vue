033d727ff9b3a41b4048081db3a19d1f90eec5a8
payushthapa <mingsonglimbu@gmail.com>
2023-06-25 14:59:41 +0545

    product edit

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,15 +1,31 @@
 <template>
     <div>
-      <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <img :src="product.image[0]" alt="" height="400px">
-                <h1>{{ product.title }}</h1>
-                <p>{{ product.description }}</p>
-                <p>{{ product.prize }}</p>
-            </div>
+     <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <img :src="product.images[0]" alt="" class="img-fluid">
+        </div>
+        <div>
+          <h1>{{ product.title }}</h1>
+        </div>
+        <div>
+          <p>{{ product.description }}</p>
+        </div>
+        <div>
+          <p>${{ product.price }}</p>
+        </div>
+        <div>
+          <h6>Category</h6>
+          <p>{{ product.category.name }}</p>
+          <img :src="product.category.image" alt="" width="80" class="rounded">
+        </div>
+        <div>
+          <span><i class="fa-solid fa-circle-minus fs-1"></i></span>
+          <span class="fs-1 px-5"></span>
+          <span><i class="fa-solid fa-circle-plus fs-1"></i></span>
         </div>
       </div>
+     </div>
     </div>
 </template>
 
